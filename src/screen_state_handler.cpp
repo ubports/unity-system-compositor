3032da6338b2100282f659e7b2e2ba794673101e
Robert Carr <robert.carr@canonical.com>
2014-09-04 15:59:23 -0700

    Implement dbus interface for touch visualizer

--- src/screen_state_handler.cpp
+++ src/screen_state_handler.cpp
@@ -22,6 +22,7 @@
 #include <mir/default_server_configuration.h>
 #include <mir/graphics/display.h>
 #include <mir/graphics/display_configuration.h>
+#include <mir/input/touch_visualizer.h>
 
 #include <cstdio>
 #include "dbus_screen.h"
@@ -227,5 +228,10 @@ void ScreenStateHandler::dimmer_alarm_notification()
 void ScreenStateHandler::set_touch_visualization_enabled(bool enabled)
 {
     std::lock_guard<std::mutex> lock{guard};
-    (void) enabled;
+    
+    auto visualizer = config->the_touch_visualizer();
+    if (enabled)
+        visualizer->enable();
+    else
+        visualizer->disable();
 }
