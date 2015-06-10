44d06c48bac7e1e86918c5504703737bb1d61cba
Alexandros Frantzis <alexandros.frantzis@canonical.com>
2015-06-10 17:38:43 +0300

    spinner: Specify surface size when drawing on a specific output

--- spinner/eglapp.cpp
+++ spinner/eglapp.cpp
@@ -219,6 +219,15 @@ std::vector<std::shared_ptr<MirEglSurface>> mir_eglapp_init(int argc, char *argv
     // If an output has been specified just do that
     if (surfaceparm.output_id != mir_display_output_id_invalid)
     {
+        for_each_active_output(connection, [&](MirDisplayOutput const* output)
+            {
+                if (output->output_id == surfaceparm.output_id)
+                {
+                    auto const& mode = output->modes[output->current_mode];
+                    surfaceparm.width = mode.horizontal_resolution;
+                    surfaceparm.height = mode.vertical_resolution;
+                }
+            });
         result.push_back(std::make_shared<MirEglSurface>(mir_egl_app, surfaceparm, swapinterval));
         return result;
     }
