d7a7a4368dbfa0bca0739eba8f63a44cb91b7e15
Mirco Müller <mirco.mueller@canonical.com>
2015-06-30 14:04:53 +0200

    Totally forgot to enable trilinear texture-filtering.

--- spinner/eglspinner.cpp
+++ spinner/eglspinner.cpp
@@ -161,9 +161,10 @@ void uploadTexture (GLuint id, Image& image)
                  GL_UNSIGNED_BYTE,
                  image.pixel_data);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glGenerateMipmap(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, 0);
 }
 
