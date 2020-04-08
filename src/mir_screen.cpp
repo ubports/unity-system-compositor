0b928b800cfcb7452b5ebde1f805d0c5d709114c
Ratchanan Srirattanamet <ratchanan@ubports.com>
2020-04-08 18:55:31 +0700

    Screen: also calls handlers on turning on/off displays
    Turning on/off display can be considered a configuration change, and
    other people might want to know about that. Note that this changes the
    meaning of the "ActiveOutputs" property exposed on DBus.

--- src/mir_screen.cpp
+++ src/mir_screen.cpp
@@ -246,6 +246,9 @@ try
 
     if (has_active_outputs(*displayConfig))
         compositor->start();
+
+    // Setting power mode is considered a configuration change.
+    configuration_applied(displayConfig);
 }
 catch (std::exception const&)
 {
